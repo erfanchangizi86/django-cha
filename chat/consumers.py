2a14c5a3df564edf9b452223b32c0ef0c87a1e57
erfanchangizi86 <erfanchangizi401@gmail.com>
2025-01-02 11:56:41 +0330

    test

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,26 +1,40 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer,WebsocketConsumer
 import json
+from  asgiref.sync  import async_to_sync
+class ChatConsumer(WebsocketConsumer):
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+    def new_message(self,data):
+        print('hello')
+
+    def fetch_message(self):
+        pass
+
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+        self.accept()
+    commands = {
+        'new_message':new_message,
+        'fetch_message': fetch_message,
+    }
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        await self.channel_layer.group_send(
+        command = text_data_json['command']
+
+        self.commands[command](self,message)
+        
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -28,8 +42,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event['message']
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': "You: "+message
         }))
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+user = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(user,verbose_name='نویسنده',on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='متن')
+    time = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ')
